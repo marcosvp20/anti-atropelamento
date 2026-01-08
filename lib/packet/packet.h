4d78f12eb91160654903b10025c5674655ba2f3d
NiefTrak <grezende.oliv@gmail.com>
2026-01-08 15:46:42 -0300

    Atualizando packet para calculo dos quadrantes de risco

--- lib/packet/packet.h
+++ lib/packet/packet.h
@@ -19,6 +19,12 @@ struct safetyData {
     uint8_t deviceType;
     float latitude;
     float longitude;
+    //trocar lat e lng float por int32_t
+    // int32_t  lat;          // 4 bytes (coordenada multiplicada por 10^6 para evitar float)
+    // int32_t  lng;          // 4 bytes
+    uint16_t course;        // 2 bytes (0-360 em centésimos)
+    uint8_t  RiskRadius;    // 1 byte  (em metros)
+    uint8_t  DangerMask;// 1 byte  (A máscara de bits)
 };
 struct monitoringData {
     uint8_t packetID;
@@ -26,6 +32,9 @@ struct monitoringData {
     uint8_t deviceType;
     float latitude;
     float longitude;
+    //trocar lat e lng float por int32_t
+    // int32_t  lat;          // 4 bytes (coordenada multiplicada por 10^6 para evitar float)
+    // int32_t  lng;          // 4 bytes
     uint8_t batteryLevel;
     float last5positions[5][2];
     uint8_t last5events[5];
